f0ba27aee7d5d34bc64642b4955537f3946fee72
behrya <behrya@e8761022-d16b-44b9-b4d4-61dd033994a1>
2007-08-16 02:31:38 +0000

    initial revision
    git-svn-id: http://svn.geo.vuw.ac.nz/svn/noise_correlation/noise_correlation/auto/trunk@254 e8761022-d16b-44b9-b4d4-61dd033994a1

---
+++ check-resp.py
@@ -0,0 +1,121 @@
+"""module to modify start and end date of RESP-files according to the
+corresponding SAC-files
+$Rev$
+$Author$
+$LastChangedDate$
+"""
+import sys
+sys.path.append('./modules')
+import  os, os.path, string, pysacio
+
+class CheckResp:
+    """class to check and, if necessary, modify response files"""
+
+
+    def check_resp(self, file1):
+        try:
+            stat1 = os.stat(file1)
+        except Exception, e:
+            "Cannot check resp-file ", file1
+        else:
+            try:
+                dirname, basename = os.path.split(file1)
+                explode = string.split(basename,'.')
+            except Exception, e:
+                print e
+            else:
+                try:
+                    for ffile in os.listdir(dirname):
+                        if string.find(ffile, 'SAC')!=-1 and string.find(ffile, explode[2])!=-1:
+                            ffile = dirname+'/'+ffile
+                            [hf,hi,hs,ok] = pysacio.ReadSacHeader(ffile)
+                            if ok == 0:
+                                message = "Cannot read in SAC-file-header"
+                                raise Exception, message
+                except Exception,e:
+                    print "Error in pysacio-part: ", message
+                else:
+                    try:
+                        tempfile = file1+'_tmp'
+                        output = open(tempfile,'w')
+                        input = open(file1,'r')
+                        outlines = []
+                    except Exception,e:
+                        print "problems to open input/output-files ",e
+                    else:
+                        try:
+                            # processing the RESP-file
+                            for lines in input.readlines():
+                                if string.find(lines,'Start date')!=-1:
+                                    date_resp1 = string.split(lines)
+                                    date_resp2 = string.split(date_resp1[3],',')
+                                    # following line-layout is exactly the one
+                                    # from the seed-RESP-file;
+                                    hi[1] = hi[1]-1
+                                    if hi[1]<10:
+                                        yday = '00'+`hi[1]`
+                                    elif hi[1]<100:
+                                        yday = '0'+`hi[1]`
+                                    else:
+                                        yday = `hi[1]`
+                                    newline = date_resp1[0]+'     '+date_resp1[1]+\
+                                              ' '+date_resp1[2]+'  '+`hi[0]`+','+\
+                                              yday+','+date_resp2[2]+'\n'
+                                    outlines.append(newline)
+                                elif string.find(lines,'No Ending Time')!=-1:
+                                    outlines.append(lines)
+                                elif string.find(lines,'End date')!=-1:
+                                    date_resp1 = string.split(lines)
+                                    date_resp2 = string.split(date_resp1[3],',')
+                                    # following line-layout is exactly the one
+                                    # from the seed-RESP-file;
+                                    hi[1] = hi[1]+2
+                                    if hi[1]<10:
+                                        yday = '00'+`hi[1]`
+                                    elif hi[1]<100:
+                                        yday = '0'+`hi[1]`
+                                    else:
+                                        yday = `hi[1]`
+                                    newline = date_resp1[0]+'     '+date_resp1[1]+\
+                                              ' '+date_resp1[2]+'    '+`hi[0]`+','\
+                                              +yday+','+date_resp2[2]+'\n'
+                                    outlines.append(newline)
+                                else:
+                                    outlines.append(lines)
+                        except Exception,e:
+                            print "problems with processing the RESP-files ",e
+                        else:
+                            try:
+                                # writing new temporary RESP-file and
+                                # copying it to the original one
+                                input.close()
+                                output.writelines(outlines)
+                                output.close()
+                                os.rename(tempfile,file1)
+                            except Exception,e:
+                                print "problems with closing or writing ",e
+                        
+    def walk_dir(self, sac1):
+        try:
+            print "checking: ", sac1
+            os.path.isdir(sac1)
+        except Exception, e:
+            print "ERROR: directory doesn't exist: ", e
+        else:
+            try:
+                list1 = os.listdir(sac1)
+                for a in list1:
+                    newsac1 = sac1+'/'+a
+                    if string.find(a, 'SAC')!=-1:                        
+                        pass
+                    elif string.find(a, 'RESP')!=-1:
+                        #print a
+                        self.check_resp(newsac1)
+                    else:
+                        self.walk_dir(newsac1)
+            except Exception, e:
+                print "ERROR in comparing lists", e
+
+if __name__ == '__main__':
+    test = CheckResp()
+    test.walk_dir('../nord-sac2/2003')

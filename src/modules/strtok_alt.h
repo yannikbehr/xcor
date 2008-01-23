230e67e26ffa6ca617625fdfff877ad6790010a0
(no author) <(no author)@e8761022-d16b-44b9-b4d4-61dd033994a1>
2008-01-23 04:18:05 +0000

    another merge from the tasman branch to update src/modules dir with strtok_alt changes
    git-svn-id: http://svn.geo.vuw.ac.nz/svn/noise_correlation/auto/trunk@473 e8761022-d16b-44b9-b4d4-61dd033994a1

---
+++ src/modules/strtok_alt.h
@@ -0,0 +1,9 @@
+#ifndef _STRTOK_ALT_H_
+#define _STRTOK_ALT_H_
+#include <stdio.h>              
+#include <stdlib.h>                    
+#include <string.h>                    
+
+int ex_tokens(char *string, char c, char ***tokens);
+
+#endif

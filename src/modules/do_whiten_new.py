47db7ddde9c0a1d3eb7105194a4b5b7475b9634b
(no author) <(no author)@e8761022-d16b-44b9-b4d4-61dd033994a1>
2008-06-11 04:10:20 +0000

    changed overall processing structure
    git-svn-id: http://svn.geo.vuw.ac.nz/svn/noise_correlation/auto/trunk@573 e8761022-d16b-44b9-b4d4-61dd033994a1

--- src/modules/do_whiten_new.py
+++ src/modules/do_whiten_new.py
@@ -9,6 +9,8 @@ obtaining tempdir-name from config-file
 import os, os.path, string, shutil, glob, sys
 from ConfigParser import SafeConfigParser
 
+import pysacio as p
+
 class TwirlyBar:
     """show progress of program"""
     def __init__(self):
@@ -39,12 +41,11 @@ class DoWhiten:
         self.upperp = int(conf.get("processing", "upperperiod"))
         self.lowerp = int(conf.get("processing", "lowerperiod"))
         self.eqband = [50, 15]
-        #self.months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-        self.months = ['Aug']
+        self.months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 	self.cnffilename = cnffile
         self.npow = 1
         self.tb = TwirlyBar()
-        self.complst = ['LHZ']
+        self.complst = ['BHE', 'BHN']
         self.proclst = ProcLst()
         self.proclst.ydaydir = []
         self.cnt = -1
@@ -59,12 +60,14 @@ class DoWhiten:
         filtercmd = self.bindir+"/filter4"
         child = os.popen(filtercmd, 'w')
         for i in self.proclst.ydaydir:
-            for j in self.complst:
-                for k in i[j]:
-                    src, tar, eqtar = k
-                    self.tb.ShowProgress()
-                    print >>child, ltaper, lowerp, upperp, utaper, self.npow, src, tar
-                    print >>child, eqltaper, self.eqband[0], self.eqband[1], equtaper, self.npow, tar, eqtar
+            print i['name']
+            self.tb.ShowProgress()
+            if len(i.keys()) > 1:
+                for j in self.complst:
+                    for k in i[j]:
+                        src, tar, eqtar = k
+                        print >>child, ltaper, lowerp, upperp, utaper, self.npow, src, tar
+                        print >>child, eqltaper, self.eqband[0], self.eqband[1], equtaper, self.npow, tar, eqtar
         err = child.close()
         if err:
             raise RuntimeError, '%r failed with exit code %d' %(filtercmd, err)
@@ -77,80 +80,116 @@ class DoWhiten:
         """ call sac routines conduct whitening for Z-component"""
         utaper = upperp - (float(upperp)/100)*20
         ltaper = lowerp + (float(lowerp)/100)*20
-        saccmd = self.sacbin+'/sac'+' 2>/dev/null 1>/dev/null'
+        saccmd = self.sacbin+'/sac'+' 1>/dev/null'
         child1 = os.popen(saccmd, 'w')
-        whitefilter = self.bindir+'/white_1cmp'+' 2>/dev/null 1>/dev/null'
+        whitefilter = self.bindir+'/white_1cmp'+' 1>/dev/null'
         child2 = os.popen(whitefilter, 'w')
         for i in self.proclst.ydaydir:
-            for j in self.complst:
-                for k in i[j]:
-                    src, tar, eqtar = k
-                    self.tb.ShowProgress()
-                    print >>child1, "r %s" %(eqtar)
-                    print >>child1, "abs"
-                    print >>child1, "smooth mean h 128"
-                    print >>child1, "w a1.avg"
-                    print >>child1, "r %s" %(tar)
-                    print >>child1, "divf a1.avg"
-                    print >>child1, "w over %s" %(tar)
-                    if os.path.isfile('a1.avg'):
-                        os.unlink('a1.avg')
-                    self.tb.ShowProgress()
-                    print >>child2, ltaper, lowerp, upperp, utaper, self.npow, tar
+            if len(i.keys()) > 1:
+                for j in self.complst:
+                    for k in i[j]:
+                        src, tar, eqtar = k
+                        self.tb.ShowProgress()
+                        print >>child1, "r %s" %(eqtar)
+                        print >>child1, "abs"
+                        print >>child1, "smooth mean h 128"
+                        print >>child1, "w over a1.avg"
+                        print >>child1, "r %s" %(tar)
+                        print >>child1, "divf a1.avg"
+                        print >>child1, "w over %s" %(tar)
+                        self.tb.ShowProgress()
+                        print >>child2, ltaper, lowerp, upperp, utaper, self.npow, tar
         err1 = child1.close()
         err2 = child2.close()
+        if os.path.isfile('a1.avg'):
+            os.remove('a1.avg')
         if err1:
-            raise RuntimeError, '%r failed with exit code %d' %(saccmd, err)
+            raise RuntimeError, '%r failed with exit code %d' %(saccmd, err1)
             return 0
         if err2:
-            raise RuntimeError, '%r failed with exit code %d' %(whitefilter, err)
+            raise RuntimeError, '%r failed with exit code %d' %(whitefilter, err2)
             return 0
         else:
             return 1
 
 
-    def white_2_comp(self, fileinEeqb, fileinNeqb, fileinE, fileinN, \
-                     fileoutE, fileoutN, upperp, lowerp):
+    def xtract_fn(self, file1, file2):
+        """check if 2 sacfiles have same station name and different horiz.
+        components (E and N)"""
+        [hf1, hi1, hs1, ok1] = p.ReadSacHeader(file1)
+        [hf2, hi2, hs2, ok2] = p.ReadSacHeader(file2)
+        if ok1:
+            stat1 = string.strip(p.GetHvalue('kstnm',hf1,hi1,hs1))
+            comp1 = string.strip(p.GetHvalue('kcmpnm',hf1,hi1,hs1))
+        else:
+            print "ERROR: cannot read in sac-header for file: ", src1
+            return 0
+        if ok2:
+            stat2 = string.strip(p.GetHvalue('kstnm',hf2,hi2,hs2))
+            comp2 = string.strip(p.GetHvalue('kcmpnm',hf2,hi2,hs2))
+        else:
+            print "ERROR: cannot read in sac-header for file: ", src2
+            return 0
+        if stat1 == stat2:
+            if comp1.endswith('N') and comp2.endswith('E'):
+                return 1
+            elif comp1.endswith('E') and comp2.endswith('N'):
+                return 1
+        else:
+            print "ERROR: station names are not the same"
+            return 0
+
+
+    def white_2_comp(self, upperp, lowerp):
         """ call sac routines conduct whitening for North and East component"""
-        self.tb.ShowProgress()
         utaper = upperp - (float(upperp)/100)*20
         ltaper = lowerp + (float(lowerp)/100)*20
-        saccmd = self.sacbin+'/sac'
-        child = os.popen(saccmd, 'w')
-        print >>child, "r %s %s" %(fileinEeqb, fileinNeqb)
-        print >>child, "abs"
-        print >>child, "smooth mean h 128"
-        print >>child, "w aaa bbb"
-        print >>child, "r aaa"
-        print >>child, "subf bbb"
-        print >>child, "abs"
-        print >>child, "addf aaa"
-        print >>child, "addf bbb"
-        print >>child, "div 2"
-        print >>child, "w a1.avg"
-        print >>child, "r %s %s" %(fileinE, fileinN)
-        print >>child, "divf a1.avg"
-        print >>child, "w over %s %s" %(fileoutE, fileoutN)
-        err = child.close()
-        if os.path.isfile('aaa'):
-            os.unlink('aaa')
-        if os.path.isfile('bbb'):
-            os.unlink('bbb')
-        if os.path.isfile('a1.avg'):
-            os.unlink('a1.avg')
+        saccmd = self.sacbin+'/sac 1 > /dev/null'
+        whitefilter = self.bindir+'/white_2cmp 1 > /dev/null'
+        child1 = os.popen(saccmd, 'w')
+        child2 = os.popen(whitefilter, 'w')
+        for i in self.proclst.ydaydir:
+            self.tb.ShowProgress()
+            if not (len(i.keys()) == 3):
+                print 'ERROR: too many components or not enough'
+                continue
+            else:
+                for j,k in zip(i[self.complst[0]], i[self.complst[1]]):
+                    src1, tar1, eqtar1 = j
+                    src2, tar2, eqtar2 = k
+                    if not self.xtract_fn(src1, src2):
+                        print "ERROR: files %s and %s inadequate!" %(src1, src2)
+                    else:
+                        print src1, src2
+                        print >>child1, "r %s %s" %(eqtar1, eqtar2)
+                        print >>child1, "abs"
+                        print >>child1, "smooth mean h 128"
+                        print >>child1, "w over aaa bbb"
+                        print >>child1, "r aaa"
+                        print >>child1, "subf bbb"
+                        print >>child1, "abs"
+                        print >>child1, "addf aaa"
+                        print >>child1, "addf bbb"
+                        print >>child1, "div 2"
+                        print >>child1, "w over a1.avg"
+                        print >>child1, "r %s %s" %(tar1, tar2)
+                        print >>child1, "divf a1.avg"
+                        print >>child1, "w over %s %s" %(tar1, tar2)
+                        print >>child2, ltaper, lowerp, upperp, utaper, self.npow, tar1, tar2
+        if os.path.isfile('./aaa'):
+            os.remove('./aaa')
+        if os.path.isfile('./bbb'):
+            os.remove('./bbb')
+        if os.path.isfile('./a1.avg'):
+            os.remove('./a1.avg')
+        err = child1.close()
         if err:
             raise RuntimeError, '%r failed with exit code %d' %(saccmd, err)
-            return 0
+        err = child2.close()
+        if err:
+            raise RuntimeError, '%r failed with exit code %d' %(whitefilter, err)
         else:
-            whitefilter = self.bindir+'/white_2cmp'
-            child = os.popen(whitefilter, 'w')
-            print >>child, ltaper, lowerp, upperp, utaper, self.npow, fileoutE, fileoutN
-            err = child.close()
-            if err:
-                raise RuntimeError, '%r failed with exit code %d' %(whitefilter, err)
-                return 0
-            else:
-                return 1
+            return 1
 
 
     def create_dir_struct(self, dirname, month):
@@ -176,22 +215,30 @@ class DoWhiten:
                 # get all ft-files with channel name given in complst;
                 # write them into strct
                 for i in range(0,len(self.complst)):
-                    tmplist = glob.glob(dirname+'/'+month+'/'+day+'/ft_*'+self.complst[i]+'*')
-                    self.proclst.ydaydir[self.cnt][self.complst[i]] = []
-                    for file in tmplist:
-                        file = os.path.basename(file)
-                        src = dirname+'/'+month+'/'+day+'/'+file
-                        tar = dirname+'/'+month+'/'+bpfile+'/'+day+'/'+file
-                        eqtar = dirname+'/'+month+'/'+bpfile+'/'+day+'/eqband/'+file
-                        self.proclst.ydaydir[self.cnt][self.complst[i]].append((src,tar,eqtar))
+                    tmplist = glob.glob(dirname+'/'+month+'/'+day+'/ft_*'+\
+                                        self.complst[i]+'*')
+                    if len(tmplist) > 0:
+                        self.proclst.ydaydir[self.cnt][self.complst[i]] = []
+                        for file in tmplist:
+                            file = os.path.basename(file)
+                            src = dirname+'/'+month+'/'+day+'/'+file
+                            tar = dirname+'/'+month+'/'+bpfile+'/'+day+'/'+file
+                            eqtar = dirname+'/'+month+'/'+bpfile+'/'+day+'/eqband/'+file
+                            self.proclst.ydaydir[self.cnt][self.complst[i]].append((src,tar,eqtar))
 
 
     def process(self):
-        print "processing...."
-        #self.filter4_f(self.upperp, self.lowerp)
+        print "filtering...."
+        self.filter4_f(self.upperp, self.lowerp)
         if len(self.complst) < 2:
+            print "whitening...."
             self.white_1_comp(self.upperp, self.lowerp)
-
+        elif len(self.complst) == 2:
+            print "whitening...."
+            self.white_2_comp(self.upperp, self.lowerp)
+        else:
+            print "ERROR: number of components too big or too small!"
+            sys.exit(1)
 
     def dir_walk(self, arg, dirname, names):
         """ called by os.path.walk; checks if the directory 'dirname\n

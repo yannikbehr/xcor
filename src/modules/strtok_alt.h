b0a83ef9b92dd84b16392ef7d8b73686b1aee4c6
(no author) <(no author)@e8761022-d16b-44b9-b4d4-61dd033994a1>
2009-03-10 02:21:49 +0000

    removed strtok_*-functions as they are deprecated
    git-svn-id: http://svn.geo.vuw.ac.nz/svn/noise_correlation/auto/trunk@709 e8761022-d16b-44b9-b4d4-61dd033994a1

--- src/modules/strtok_alt.h
+++
@@ -1,9 +0,0 @@
-#ifndef _STRTOK_ALT_H_
-#define _STRTOK_ALT_H_
-#include <stdio.h>              
-#include <stdlib.h>                    
-#include <string.h>                    
-
-int ex_tokens(char *string, char c, char ***tokens);
-
-#endif

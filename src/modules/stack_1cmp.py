8e63cb38de89c768ad10a2ee78197082f756d745
(no author) <(no author)@e8761022-d16b-44b9-b4d4-61dd033994a1>
2009-03-10 02:19:45 +0000

    made stack_1cmp.py modular so it can be called from other scripts
    git-svn-id: http://svn.geo.vuw.ac.nz/svn/noise_correlation/auto/trunk@708 e8761022-d16b-44b9-b4d4-61dd033994a1

--- src/modules/stack_1cmp.py
+++ src/modules/stack_1cmp.py
@@ -9,45 +9,109 @@ from ConfigParser import SafeConfigParser
 sys.path.append('/home/behrya/dev/proc-scripts')
 import delaz as dz
 
-mystack = {}
+class PAR: pass
 
-def find_match(f,spattern):
-    match = re.search(spattern,f)
-    if match:
-        [hf,hi,hs,seis,ok] = p.ReadSacFile(f)
-        if not ok:
-            print "ERROR: cannot read sac-file %s" %(f)
-            return -1
-        else:
-            trace = np.array(seis, dtype=float)
-            if match.group(1) in mystack.keys():
-                oldtrace = np.array(mystack[match.group(1)]['trace'], dtype=float)
-                newtrace = oldtrace+trace
-                if len(newtrace) > 0:
-                    new = {}
-                    new[match.group(1)] = {}
-                    new[match.group(1)]['trace'] = newtrace
-                    new[match.group(1)]['hf'] = hf
-                    new[match.group(1)]['hs'] = hs
-                    new[match.group(1)]['hi'] = hi
-                    mystack.update(new)
-                    return 1
-                else: return -1
-            else:
-                mystack[match.group(1)] = {}
-                mystack[match.group(1)]['trace'] = trace
-                mystack[match.group(1)]['hf'] = hf
-                mystack[match.group(1)]['hs'] = hs
-                mystack[match.group(1)]['hi'] = hi
-                return 1
-    else:
-        return 0
-    
-def find_xcor(spattern, dirname, files):
+def find_match(par,dirname,filelist):
+    """find 'COR'-files in 'dirname' and add them to global dict of 'COR'-files """
     corfiles = glob.glob(dirname+'/COR*')
     if len(corfiles) > 0:
         for f in corfiles:
-            find_match(f,spattern)
+            match = re.search(par.spattern,f)
+            if match:
+                [hf,hi,hs,seis,ok] = p.ReadSacFile(f)
+                if not ok:
+                    print "ERROR: cannot read sac-file %s" %(f)
+                    return -1
+                trace = np.array(seis, dtype=float)
+                nstack = p.GetHvalue('mag',hf,hi,hs)
+                stat1 = match.group(1)
+                stat2 = match.group(2)
+                # account for the fact that the correlation could be
+                # either COR_MATA_TIKO.SAC or COR_TIKO_MATA.SAC
+                comb1=stat1+'_'+stat2
+                comb2=stat2+'_'+stat1
+                if comb1 in par.mystack.keys():
+                    oldtrace = np.array(par.mystack[comb1]['trace'], dtype=float)
+                    newtrace = oldtrace+trace
+                    if len(newtrace) > 0:
+                        oldstack = p.GetHvalue('mag',par.mystack[comb1]['hf'],par.mystack[comb1]['hi'],
+                                               par.mystack[comb1]['hs'])
+                        newstack = nstack+oldstack
+                        p.SetHvalue('mag',newstack,hf,hi,hs)
+                        new = {}
+                        new[comb1] = {}
+                        new[comb1]['trace'] = newtrace
+                        new[comb1]['hf'] = hf
+                        new[comb1]['hs'] = hs
+                        new[comb1]['hi'] = hi
+                        par.mystack.update(new)
+                        continue
+                    else: return -1
+                elif comb2 in par.mystack.keys():
+                    oldtrace = np.array(par.mystack[comb2]['trace'], dtype=float)
+                    newtrace = oldtrace+trace[::-1]
+                    if len(newtrace) > 0:
+                        oldstack = p.GetHvalue('mag',par.mystack[comb2]['hf'],par.mystack[comb2]['hi'],
+                                               par.mystack[comb2]['hs'])
+                        newstack = nstack+oldstack
+                        p.SetHvalue('mag',newstack,hf,hi,hs)
+                        new = {}
+                        new[comb2] = {}
+                        new[comb2]['trace'] = newtrace
+                        new[comb2]['hf'] = hf
+                        new[comb2]['hs'] = hs
+                        new[comb2]['hi'] = hi
+                        par.mystack.update(new)
+                        continue
+                    else: return -1
+                else:
+                    par.mystack[comb1] = {}
+                    par.mystack[comb1]['trace'] = trace
+                    par.mystack[comb1]['hf'] = hf
+                    par.mystack[comb1]['hs'] = hs
+                    par.mystack[comb1]['hi'] = hi
+                    continue
+
+    return 0
+
+
+def write_stack(stackdir,par):
+    """write contents of global 'COR'-file dict to disk"""
+    if not os.path.isdir(stackdir):
+        os.mkdir(stackdir)
+    for stat in par.mystack.keys():
+        # write stacked correlation
+        seis = par.mystack[stat]['trace']
+        hf = par.mystack[stat]['hf']
+        hs = par.mystack[stat]['hs']
+        hi = par.mystack[stat]['hi']
+        stat1, stat2 = stat.split('_')
+        p.SetHvalue('kevnm',stat1, hf,hi,hs)
+        p.SetHvalue('kstnm',stat2, hf,hi,hs)
+        b = p.GetHvalue('b',hf,hi,hs)
+        lat1 = p.GetHvalue('evla',hf,hi,hs)
+        lon1 = p.GetHvalue('evlo',hf,hi,hs)
+        lat2 = p.GetHvalue('stla',hf,hi,hs)
+        lon2 = p.GetHvalue('stlo',hf,hi,hs)
+        dist, dump1, dump2 = dz.delaz(lat1,lon1,lat2,lon2,0)
+        dist = dist*math.pi*6372/180
+        p.SetHvalue('dist',dist,hf,hi,hs)
+        outputfile = stackdir+'/COR_'+stat+'.SAC'
+        print 'writing',outputfile
+        p.WriteSacBinary(outputfile, hf, hi, hs, a.array('f',seis))
+
+        # write symmetric part 
+        delta = p.GetHvalue('delta',hf,hi,hs)
+        null = -1*b/delta
+        reversed = seis[::-1]
+        newseis = seis+reversed
+        p.SetHvalue('npts',len(newseis[null:]),hf,hi,hs)
+        p.SetHvalue('b',0,hf,hi,hs)
+        p.SetHvalue('o',0, hf,hi,hs)
+        outputfile = stackdir+'/COR_'+stat+'.SAC_s'
+        print 'writing',outputfile
+        p.WriteSacBinary(outputfile, hf, hi, hs, a.array('f',newseis[null:]))
+
 
 if __name__ == '__main__':
 
@@ -67,39 +131,8 @@ if __name__ == '__main__':
         print "no configuration file found"
         sys.exit(1)
 
-    os.path.walk(rootdir, find_xcor, spattern)
-    if not os.path.isdir(stackdir):
-        os.mkdir(stackdir)
-    for stat in mystack.keys():
-        #print mystack[stat]
-        # write stacked correlation
-        seis = mystack[stat]['trace']
-        hf = mystack[stat]['hf']
-        hs = mystack[stat]['hs']
-        hi = mystack[stat]['hi']
-        stat1, stat2 = stat.split('_')
-        p.SetHvalue('kevnm',stat1, hf,hi,hs)
-        p.SetHvalue('kstnm',stat2, hf,hi,hs)
-        b = p.GetHvalue('b',hf,hi,hs)
-        lat1 = p.GetHvalue('evla',hf,hi,hs)
-        lon1 = p.GetHvalue('evlo',hf,hi,hs)
-        lat2 = p.GetHvalue('stla',hf,hi,hs)
-        lon2 = p.GetHvalue('stlo',hf,hi,hs)
-        dist, dump1, dump2 = dz.delaz(lat1,lon1,lat2,lon2,0)
-        dist = dist*math.pi*6372/180
-        p.SetHvalue('dist',dist,hf,hi,hs)
-        outputfile = stackdir+'/COR_'+stat+'.SAC'
-        print 'writing',outputfile
-        p.WriteSacBinary(outputfile, hf, hi, hs, a.array('f',seis))
-
-        # write causal part only
-        delta = p.GetHvalue('delta',hf,hi,hs)
-        null = -1*b/delta
-        reversed = seis[-1::-1]
-        newseis = seis+reversed
-        p.SetHvalue('npts',len(newseis[null:]),hf,hi,hs)
-        p.SetHvalue('b',0,hf,hi,hs)
-        p.SetHvalue('o',0, hf,hi,hs)
-        outputfile = stackdir+'/COR_'+stat+'.SAC_s'
-        print 'writing',outputfile
-        p.WriteSacBinary(outputfile, hf, hi, hs, a.array('f',newseis[null:]))
+    par = PAR()
+    par.mystack = {}
+    par.spattern = spattern
+    os.path.walk(rootdir, find_match, par)
+    write_stack(stackdir,par)

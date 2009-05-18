3cb05ae67169cf98e9601f2b7faf00b5a081930c
(no author) <(no author)@e8761022-d16b-44b9-b4d4-61dd033994a1>
2009-05-18 22:15:04 +0000

    changed code structure so ftan can be called as a module from global driver
    git-svn-id: http://svn.geo.vuw.ac.nz/svn/noise_correlation/auto/trunk@757 e8761022-d16b-44b9-b4d4-61dd033994a1

--- src/FTAN/pv/run_ftanpv.py
+++ src/FTAN/pv/run_ftanpv.py
@@ -8,128 +8,94 @@ import pysacio as p
 import ftanpv
 from pylab import *
 
-####################### FTAN PARAMETERS #######################
-t0      = 0
-nfin    = 32
-npoints = 10
-perc    = 50.0
-dt      = 1
-vmin    = 1.
-vmax    = 5.
-tmin    = 4.
-tmax    = 40.
-thresh  = 20.
-taperl  = 0.5
-snr     = 0.2
-fmatch  = 2.0
-piover4 = -1
-ffact   = 1
 
+def myftan(fn,ref,t0=0,nfin=32,npoints=10,perc=50.0,dt=1.,vmin=1.,
+           vmax=5.,tmin=4,tmax=None,thresh=20,ffact=1.,taperl=.5,snr=0.2,
+           fmatch=2,piover4=-1,phm=True,steps=False):
 
-############## result from fanchi's code 1st FTAN run ######
-compdisp = './COR_GSC_R06C.SAC_s_1_DISP.1'
-f = open(compdisp,'r')
-vel = []; per = []
-for i in f.readlines():
-    a=i.split()
-    per.append(a[1])
-    vel.append(a[3])
-f.close()
+    [hf,hi,hs,seis,ok] = p.ReadSacFile(fn)
+    stat1 = string.rstrip(p.GetHvalue('kstnm',hf,hi,hs))
+    stat2 = string.rstrip(p.GetHvalue('kevnm',hf,hi,hs))
+    n = p.GetHvalue('npts',hf,hi,hs)
+    delta = p.GetHvalue('dist',hf,hi,hs)
+    times = arange(int(delta/vmax),int(delta/vmin))
+    vels  = [ delta/i for i in times]
+    if tmax==None:
+        tmax = delta/(2*vmax)
+        if tmax > 35:
+            tmax = 35
 
-############## result from fanchi's code 2nd FTAN run ######
-compdisp = './COR_GSC_R06C.SAC_s_2_DISP.1'
-f = open(compdisp,'r')
-vel1 = []; per1 = []
-for i in f.readlines():
-    a=i.split()
-    per1.append(a[1])
-    vel1.append(a[3])
-f.close()
+    trace = zeros(32768)
+    for i in range(0,len(seis)):
+        if i < 32767:
+            trace[i] = seis[i]
+    reftr = load(ref)
+    phprper = zeros(300)
+    phprvel = zeros(300)
+    nphpr = len(reftr[:,1])
+    for i in range(0,nphpr):
+        phprper[i] = reftr[i,0]
+        phprvel[i] = reftr[i,1]
+    
+    nfout1,arr1,nfout2,arr2,tamp,nrow,ncol,amp,ierr = ftanpv.aftanpg(piover4,n,trace,t0,dt,
+                                                                     delta,vmin,vmax,tmin,
+                                                                     tmax,thresh,ffact,perc,
+                                                                     npoints,taperl,nfin,snr,
+                                                                     nphpr,phprper,phprvel)
+            
+    if ierr == 2 or ierr == 1 or nfout2 == 2:
+        raise Exception("ERROR in ftan-method (1st step)")
 
-################################## 1st FTAN run ##################################
-
-fn = './COR_GSC_R06C.SAC_s'
-[hf,hi,hs,seis,ok] = p.ReadSacFile(fn)
-stat1 = string.rstrip(p.GetHvalue('kstnm',hf,hi,hs))
-stat2 = string.rstrip(p.GetHvalue('kevnm',hf,hi,hs))
-n = p.GetHvalue('npts',hf,hi,hs)
-delta = p.GetHvalue('dist',hf,hi,hs)
-times = arange(int(delta/vmax),int(delta/vmin))
-vels  = [ delta/i for i in times]
-
-trace = zeros(32768)
-for i in range(0,len(seis)):
-    if i < 32767:
-        trace[i] = seis[i]
-
-phper = [18,20,25,29,33,40,50,67,83,100,111,125,143]
-phvel = [3.450,3.530,3.653,3.712,3.761,3.793,3.815,3.854,3.910,3.978,4.027,4.088,4.179]
-phprper = zeros(300)
-phprvel = zeros(300)
-nphpr = len(phper)
-for i in range(0,nphpr):
-    phprper[i] = phper[i]
-    phprvel[i] = phvel[i]
-
-nfout1,arr1,nfout2,arr2,tamp,nrow,ncol,amp,ierr = ftanpv.aftanpg(piover4,n,trace,t0,dt,
-                                                                 delta,vmin,vmax,tmin,
-                                                                 tmax,thresh,ffact,perc,
-                                                                 npoints,taperl,nfin,snr,
-                                                                 nphpr,phprper,phprvel)
+    if phm:
+        pred = zeros((300,2))
+        for i in range(0,nfout2):
+            pred[i][0] = arr2[1][i]
+            pred[i][1] = arr2[2][i]
+    
+        ffact = 2.0
+        fmatch = 2.0
+        npred  = nfout2
+        tmin = arr2[1][0];
+        tmax = arr2[1][nfout2-1];
+        amp   = []
+        arr1  = []
+        arr2  = []
         
-if ierr == 2 or ierr == 1 or nfout2 == 2:
-    print "ERROR in ftan "
-    exit
+        nfout1,arr1,nfout2,arr2,tamp,nrow,ncol,amp,ierr = ftanpv.aftanipg(piover4,n,trace,t0,dt,
+                                                                          delta,vmin,vmax,tmin,
+                                                                          tmax,thresh,ffact,perc,
+                                                                          npoints,taperl,nfin,
+                                                                          snr,fmatch,npred,pred,
+                                                                          nphpr,phprper,phprvel)
+    
+        if ierr == 2 or ierr == 1 or nfout2 == 2:
+            raise Exception("ERROR in ftan-method (2nd step)")
 
-subplot(2,1,1)
-x = array(arr2[0][0:nrow])
-y = array(vels)
-z = amp[0:len(vels),0:nrow]
-plot(x,arr2[2][0:nrow],'k')
-plot(per,vel,'b--')
-contourf(x,y,z,250)
-xlabel('Period [s]')
-ylabel('Group velocity [km/s]')
-
-
-
-################################## 2nd FTAN run ##################################
-
-pred = zeros((300,2))
-for i in range(0,nfout2):
-    pred[i][0] = arr2[1][i]
-    pred[i][1] = arr2[2][i]
-
-ffact = 2.0
-fmatch = 2.0
-npred  = nfout2
-tmin = arr2[1][0];
-tmax = arr2[1][nfout2-1];
-amp   = []
-arr1  = []
-arr2  = []
+    cper  = array(arr2[0][0:nrow])
+    aper  = array(arr2[1][0:nrow])
+    gv    = array(arr2[2][0:nrow])
+    pv    = array(arr2[3][0:nrow])
+    gvamp = array(arr2[4][0:nrow])
+    gvsnr = array(arr2[5][0:nrow])
+    ampv  = array(vels)
+    amps  = amp[0:len(vels),0:nrow]
+    return (cper,aper,gv,pv,gvamp,gvsnr,ampv,amps)
 
 
-nfout1,arr1,nfout2,arr2,tamp,nrow,ncol,amp,ierr = ftanpv.aftanipg(piover4,n,trace,t0,dt,
-                                                                  delta,vmin,vmax,tmin,
-                                                                  tmax,thresh,ffact,perc,
-                                                                  npoints,taperl,nfin,
-                                                                  snr,fmatch,npred,pred,
-                                                                  nphpr,phprper,phprvel)
-
-
-
-if ierr == 2 or ierr == 1 or nfout2 == 2:
-    print "ERROR in ftan "
-    exit
-
-subplot(2,1,2)
-x = array(arr2[0][0:nrow])
-y = array(vels)
-z = amp[0:len(vels),0:nrow]
-plot(x,arr2[2][0:nrow],'k')
-plot(per1,vel1,'b--')
-contourf(x,y,z,250)
-xlabel('Period [s]')
-ylabel('Group velocity [km/s]')
-show()
+if __name__=='__main__':
+    reffn = './scalifornia_avg_phvel.dat'
+    fn = './COR_GSC_R06C.SAC_s'
+    cper,aper,gv,pv,gvamp,gvsnr,ampv,amps = myftan(fn,reffn)
+    plot(aper,pv,'k')
+    plot(aper,gv,'b--')
+    contourf(aper,ampv,amps,250)
+    xlabel('Period [s]')
+    ylabel('Phase velocity [km/s]')
+    xmin, xmax = xlim()
+    ymin, ymax = ylim()
+    ############## result from fanchi's code 1st FTAN run ######
+    cmpdsp = load('./COR_GSC_R06C.SAC_s_2_DISP.1')
+    plot(cmpdsp[:,2],cmpdsp[:,3],'k+')
+    xlim(xmin,xmax)
+    ylim(ymin,ymax)
+    show()

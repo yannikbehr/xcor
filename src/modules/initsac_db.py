8afcafa6235fe25030d133aeb0276c61829da913
yannik78 <yannik78@e8761022-d16b-44b9-b4d4-61dd033994a1>
2009-10-22 22:01:01 +0000

    thin python wrapper for initsac_db so that it can be used as a function
    git-svn-id: http://svn.geo.vuw.ac.nz/svn/noise_correlation/auto/trunk@877 e8761022-d16b-44b9-b4d4-61dd033994a1

---
+++ src/modules/initsac_db.py
@@ -0,0 +1,62 @@
+#!/usr/local/bin/python
+"""
+wrapper for initsac_db C-routine
+"""
+
+import os, os.path, sys, glob
+from subprocess import *
+
+
+def initsacdb(datdir,regex='[!^ft]*.SAC',cnf='./config.txt',
+                  srchflag='0',prefix='ft',sacdbf='sac_db.out',
+                  tmpdir='./tmp/',resp_dir='./'):
+    _path = os.environ['AUTO_SRC']
+    output = open(cnf,"w")
+    outlines = []
+    outlines.append("[init_sacdb]\n")
+    outlines.append("search_directories="+datdir+"\n")
+    outlines.append("skip_directories=5to100, eqband, 1to40\n")
+    outlines.append("flag="+srchflag+"\n")
+    outlines.append("search_string="+regex+"\n")
+    outlines.append("resp_dir="+resp_dir+"\n")
+    outlines.append("prefix="+prefix+"\n")
+    outlines.append("dbname="+sacdbf+"\n")
+    outlines.append("tmpdir="+tmpdir+"\n")
+    output.writelines(outlines)
+    output.close()
+    initcmd = os.path.join(_path,'bin/initsac_db')
+    out,err = Popen([initcmd,'-c',cnf],stdout=PIPE,stderr=PIPE).communicate()
+    f = open('initsacdb.out','w')
+    f.write(out)
+    f.close()
+    f = open('initsacdb.err','w')
+    f.write(err)
+    f.close()
+   # if os.path.isfile(cnf):
+   #     os.remove(cnf)
+
+
+if __name__ == "__main__":
+    try:
+        if string.find(sys.argv[1],'-c')!=-1:
+            cnffile=sys.argv[2]
+        else:
+            print "encountered unknown command line argument"
+            raise Exception
+    except Exception:
+        cnffile = 'config.txt' 
+
+    if not os.path.isfile(cnffile):
+        print "no config file found"
+        sys.exit(1)
+
+    _path = os.environ['AUTO_SRC']
+    initcmd = os.path.join(_path,'bin/initsac_db')
+    out,err = Popen([initcmd,'-c',cnffile]).communicate()
+#    out,err = Popen([initcmd,'-c',cnffile],stdout=PIPE,stderr=PIPE).communicate()
+#    f = open('initsacdb.out','w')
+#    f.write(out)
+#    f.close()
+#    f = open('initsacdb.err','w')
+#    f.write(err)
+#    f.close()

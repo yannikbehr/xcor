d3625d43347e0b4205f43509077233628270efd1
yannik <yannik@e8761022-d16b-44b9-b4d4-61dd033994a1>
2010-03-04 05:54:42 +0000

    modified tests to match changes in whitening.py
    git-svn-id: http://svn.geo.vuw.ac.nz/svn/noise_correlation/auto/trunk@1027 e8761022-d16b-44b9-b4d4-61dd033994a1

--- src/modules/tests/test_whitening.py
+++ src/modules/tests/test_whitening.py
@@ -25,6 +25,9 @@ class WhitenTestCase(unittest.TestCase):
         tfile1eq  = os.path.join(os.path.dirname(__file__),'data/2001/Apr/2.0to90.0/2001_4_27_0_0_0/eqband/ft_grid_KNGC.BHZ.SAC')
         tfile2 = os.path.join(os.path.dirname(__file__),'data/2001/Apr/2001_4_27_0_0_0/ft_grid_KNGC.BHZ.SAC')
         tfileeq = os.path.join(os.path.dirname(__file__),'data/2001/Apr/2.0to90.0/2001_4_27_0_0_0/eqband/ft_grid_KNGC.BHZ.SAC')
+        tf = os.path.join(os.path.dirname(__file__),'data/2001/Apr/2.0to90.0/2001_4_27_0_0_0/test_ft_grid_KNGC.BHZ.SAC')
+        tamp = os.path.join(os.path.dirname(__file__),'data/2001/Apr/2.0to90.0/2001_4_27_0_0_0/test_ft_grid_KNGC.BHZ.SAC.am')
+        tph = os.path.join(os.path.dirname(__file__),'data/2001/Apr/2.0to90.0/2001_4_27_0_0_0/test_ft_grid_KNGC.BHZ.SAC.ph')
         if False:
             ### run do_whiten_new
             shutil.rmtree(os.path.join(os.path.dirname(__file__),'data/2001/Apr/2.0to90.0/'))
@@ -50,14 +53,15 @@ class WhitenTestCase(unittest.TestCase):
         sdb.rec[0][0].ft_fname = tfile2
         trace_norm = ReadSac(tfile1+'_smooth').seis
         trace_filter = ReadSac(tfile1+'_filter').seis
-        s, samp, sph, eqtr_s, ftr, ftr_norm = whitening(sdb,0,0,90.,2.,testtrace=trace_norm)
+        whitening(sdb,0,0,90.,2.,tf,testtrace=trace_norm)
         xorig = ReadSac(tfile1).seis
         xorig_amp = ReadSac(tfile1+'.am').seis
         xorig_ph = ReadSac(tfile1+'.ph').seis
-        np.testing.assert_array_almost_equal(xorig,s,decimal=6)
-        np.testing.assert_array_almost_equal(xorig_amp,samp,decimal=6)
-        np.testing.assert_array_almost_equal(trace_filter,ftr,decimal=6)
-        s, samp, sph, eqtr_s, ftr, ftr_norm = whitening(sdb,0,0,90.,2.)
+        xtest = ReadSac(tf).seis
+        xtest_amp = ReadSac(tamp).seis
+        np.testing.assert_array_almost_equal(xorig,xtest,decimal=6)
+        np.testing.assert_array_almost_equal(xorig_amp,xtest_amp,decimal=6)
+        #whitening(sdb,0,0,90.,2.,tf)
         #plot(xorig)
         #plot(s,'k--')
         #show()

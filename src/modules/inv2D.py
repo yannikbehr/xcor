4a40606eed904a2a8613622291327d5bf54e12d6
yannik <yannik@e8761022-d16b-44b9-b4d4-61dd033994a1>
2010-03-30 01:38:05 +0000

    enable raytracing; doesn't work yet
    git-svn-id: http://svn.geo.vuw.ac.nz/svn/noise_correlation/auto/trunk@1075 e8761022-d16b-44b9-b4d4-61dd033994a1

--- src/modules/inv2D.py
+++ src/modules/inv2D.py
@@ -13,9 +13,14 @@ from numpy import *
 class Inv2D:
 
     def __init__(self,cnf):
-        self.wd       = tempfile.mkdtemp()
-        self.datadir   = cnf.get('2Dmap','datadir')
-        self.ctrf      = cnf.get('2Dmap','ctrfile')
+        self.wd = tempfile.mkdtemp()
+        #self.wd = '/data/wanakaII/yannik/cnipse/inversion/checkerboard/'
+        self.datadir = cnf.get('2Dmap','datadir')
+        self.ctrf = cnf.get('2Dmap','ctrfile')
+        self.ray = cnf.getboolean('2Dmap','raytracing')
+        if self.ray:
+            self.modelf = cnf.get('2Dmap','model')
+            shutil.copy(self.modelf,self.wd)
         ### read contour file
         f1 = open(self.ctrf).readlines()
         no = int(f1[1].split()[0])
@@ -40,6 +45,9 @@ class Inv2D:
                       str(self.sigma),str(self.sigma),'7','%f'%self.latmin,'%f'%self.latmax,'%f'%self.gridlat,
                       '8','%f'%self.lonmin,'%f'%self.lonmax,'%f'%self.gridlon,'12','%f'%self.intstep,'%f'%self.cellsz,'16',
                       '19','q','v','go']
+        self.param_ray = ['me','0','2','4000','4','5','%f'%self.latmin,'%f'%self.latmax,
+                          '%f'%self.gridlat,'6','%f'%self.lonmin,'%f'%self.lonmax,'%f'%self.gridlon,
+                          '10','.1','2.0','R','G','1.','7','27','q','v','go']
         self.result    = cnf.get('2Dmap','result')
         self.tomobin   = cnf.get('2Dmap','tomobin')
         self.tomoray   = cnf.get('2Dmap','tomoray')
@@ -54,16 +62,17 @@ class Inv2D:
             self.copy_res(_p)
 
 
-    def run_inv2D(self,period,ray=False):
+    def run_inv2D(self,period,ray=False,datafile=None):
         """ function to run 2D inversion that can be called to test several
         different inversion parameters """
         ############## generate script to be run on the remote machine ####
-        datafile = os.path.join(self.datadir,'%ds_%s.txt'%(period,self.name))
+        if datafile is None:
+            datafile = os.path.join(self.datadir,'%ds_%s.txt'%(period,self.name))
         if ray:
             p = Popen('%s %s %s %d'\
                       %(self.tomoray,datafile,self.name,period),shell=True,stdin=PIPE)
             f = p.stdin
-            for _pm in self.param:
+            for _pm in self.param_ray:
                 print >>f,_pm
             f.close()
             p.wait()

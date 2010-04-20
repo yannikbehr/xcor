eeb89eb6a4c80c7986bb193dd252dead8b2c212b
yannik <yannik@e8761022-d16b-44b9-b4d4-61dd033994a1>
2010-04-20 22:25:57 +0000

    added resolution maps and misfit histograms
    git-svn-id: http://svn.geo.vuw.ac.nz/svn/noise_correlation/auto/trunk@1087 e8761022-d16b-44b9-b4d4-61dd033994a1

--- src/modules/plot_2D.py
+++ src/modules/plot_2D.py
@@ -85,7 +85,8 @@ def plot_result(**keys):
     """
     plot measurements and 2D maps
     """
-    gmt = GMT(config={'PAGE_ORIENTATION':'landscape'})
+    #gmt = GMT(config={'PAGE_ORIENTATION':'landscape'})
+    gmt = GMT()
     paths = keys['paths']
     if keys['map_range'] == None:
         lonmin,lonmax,latmin,latmax,zmin,zmax = _get_range_(paths)
@@ -102,6 +103,7 @@ def plot_result(**keys):
     ### create temporary files
     tmppath=gmt.tempfilename('paths.txt')
     fileout = keys['fileout']
+
     #### plot measurements
     if keys['plot_paths']:
         if keys['map2D'] != None:
@@ -117,12 +119,13 @@ def plot_result(**keys):
         s = guru.get_params()
         anot = 'a%ff%f/a%ff%fWSne'%(s['xinc'],s['xinc']/2.,s['yinc'],s['yinc']/2.)
         gmt.psbasemap(R=rng,J=scl,V=True,B=anot,X='2c',Y='4c',G='white')
-        gmt.pscoast(R=True,J=True,V=True,W='1/0/0/0',D='i')
         gmt.psxy(R=True,J=True,m=True,V=True,W='2',C=cptfile,in_string=_convert_result_(paths).getvalue())
+        gmt.pscoast(R=True,J=True,V=True,W='1/0/0/0',D='i')
         gmt.psscale(C=cptfile,V=True,D=scalebar,B='%f::/:km/s:'%_get_scale_tick_(zmin1,zmax1))
         textstring = """%f %f 12 0 1 LT T=%d s"""%(lonmin,latmax,keys['period'])
         gmt.pstext(R=True,J=True,D='j0.5',G='0/0/0',N=True,in_string=textstring)
 
+    ##### plot velocity maps 
     if keys['plot_map']:
         xshift = '0c'
         if keys['plot_paths']:
@@ -139,10 +142,45 @@ def plot_result(**keys):
         gmt.xyz2grd(keys['map2D'],G=grdtomotmp,I='%f/%f'%(slon,slat),R=rng,out_discard=True)
         gmt.grdsample(grdtomotmp,G=grdtomo,I='1m',Q='l',R=True,out_discard=True)
         gmt.grd2cpt(grdtomo,E=50,L='%f/%f'%(zmin,zmax),C="seis",out_filename=tomocpt)
+        if keys['colormap'] is not None:
+            tomocpt = keys['colormap']
         gmt.grdimage(grdtomo,R=True,J=scl,P=True,C=tomocpt,X=xshift)
         gmt.pscoast(R=True,J=scl,B=anot,D='i',W='thinnest' )
         gmt.psscale(C=tomocpt,D=scalebar,B='%f::/:km/s:'%_get_scale_tick_(zmin,zmax))
 
+    #### plot resolution maps
+    if keys['plot_res']:
+        xshift = '-12c'
+        yshift = '12c'
+        grdtomo = gmt.tempfilename('tomo.grd')
+        grdtomotmp = gmt.tempfilename('tomo_tmp.grd')
+        tomocpt = gmt.tempfilename('tomo.cpt')
+        lonmin,lonmax,latmin,latmax,zmin,zmax,slon,slat = _get_2dmap_lim_(map2Dres)
+        rng = '%f/%f/%f/%f'%(lonmin,lonmax,latmin,latmax)
+        if keys['map_range'] != None:
+            lonmin,lonmax,latmin,latmax = map(float,map_range.split('/'))
+            rng = map_range
+        print slon,slat,rng
+        gmt.xyz2grd(map2Dres,G=grdtomotmp,I='%f/%f'%(slon,slat),R=rng,out_discard=True)
+        gmt.grdsample(grdtomotmp,G=grdtomo,I='1m',Q='l',R=True,out_discard=True)
+        gmt.grd2cpt(grdtomo,E=50,L='%f/%f'%(0,300),C="seis",out_filename=tomocpt)
+        if keys['colormap'] is not None:
+            tomocpt = keys['colormap']
+        gmt.grdimage(grdtomo,R=True,J=scl,P=True,C=tomocpt,X=xshift,Y=yshift)
+        gmt.pscoast(R=True,J=scl,B=anot,D='i',W='thinnest' )
+        gmt.psscale(C=tomocpt,D=scalebar,B='%f::/:km:'%50)
+
+    #### plot misfit histogram
+    if keys['plot_hist']:
+        xshift = '12c'
+        yshift = '0c'
+        resid = loadtxt(residf,usecols=[7],unpack=True)
+        fstr = cStringIO.StringIO()
+        for _i in resid:
+            fstr.write("%f\n"%_i)
+        gmt.pshistogram(R="%f/%f/0/%f"%(resid.min(),resid.max(),resid.size),
+                        B="a5f5:Misfit [s]:/a50f10::WSne",J='X10c/8.5c',G='gray',
+                        W='3',L='thinner',X=xshift,Y=yshift,in_string=fstr.getvalue())
     gmt.save(fileout) 
     os.system('gv '+fileout+'&')
 
@@ -150,6 +188,7 @@ def plot_result(**keys):
 if __name__ == '__main__':
     from ConfigParser import SafeConfigParser
     map_range = None
+    colormap = None
     plot_paths = False
     plot_map = False
     try:
@@ -160,6 +199,8 @@ if __name__ == '__main__':
             cp.read(config)
             plot_paths = cp.getboolean('plotting','plot_paths')
             plot_map = cp.getboolean('plotting','plot_map')
+            plot_res = cp.getboolean('plotting','plot_res')
+            plot_hist = cp.getboolean('plotting','plot_hist')
             fileout = cp.get('plotting','fileout')
             if plot_paths:
                 pathdir = cp.get('plotting','pathdir')
@@ -172,9 +213,12 @@ if __name__ == '__main__':
                 sigma = cp.get('plotting','sigma')
                 beta = cp.get('plotting','beta')
                 period = eval(cp.get('plotting','period'))
-                
+            if plot_hist:
+                pass
             if cp.has_option('plotting','map_range'):
                 map_range = cp.get('plotting','map_range')
+            if cp.has_option('plotting','colormap'):
+                colormap = cp.get('plotting','colormap')
             
         else:
             print "encountered unknown command line argument"
@@ -187,11 +231,22 @@ if __name__ == '__main__':
         fout = '%s_%d.ps'%(fileout,_p)
         if plot_map:
             map2D = os.path.join(mapdir,str(_p),'%s_%s_%s'%(alpha,sigma,beta),'%s_%d.1'%(prefix,_p))
+            if not os.path.isfile(map2D): continue
             fout = os.path.join(mapdir,str(_p),'%s_%s_%s'%(alpha,sigma,beta),'%s_%d_%s_%s.ps'%(prefix,_p,alpha,sigma))
         else:
             map2D = None
+        if plot_res:
+            map2Dres = os.path.join(mapdir,str(_p),'%s_%s_%s'%(alpha,sigma,beta),'%s_%d.rea'%(prefix,_p))
+        else:
+            map2Dres = None
         if plot_paths:
             paths = os.path.join(pathdir,'%ss_%s.txt'%(_p,prefix))
-        plot_result(paths=paths,map2D=map2D,map_range=map_range,
-                    plot_paths=plot_paths,plot_map=plot_map,
-                    fileout=fout,period=_p)
+        else:
+            paths = None
+        if plot_hist:
+            residf = os.path.join(mapdir,str(_p),'%s_%s_%s'%(alpha,sigma,beta),'%s_%d.resid'%(prefix,_p))
+        else:
+            residf = None
+        plot_result(paths=paths,map2D=map2D,map2Dres=map2Dres,map_range=map_range,
+                    plot_paths=plot_paths,plot_map=plot_map,plot_res=plot_res,
+                    fileout=fout,period=_p,colormap=colormap,plot_hist=plot_hist,residf=residf)
